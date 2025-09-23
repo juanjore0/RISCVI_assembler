48e0ae9e734a6ccaf41895c2b7a1ae417fb8e9f6
valemr05 <valeria.munoz3@utp.edu.co>
2025-09-23 15:26:06 -0500

    memory

--- assembler.py
+++ assembler.py
@@ -1,17 +1,19 @@
 from lexer import RISCVLexer
 from parserPrincipal import RISCVParser
 from parserlabel import ParserLabel
+from memory import MemoryManager
 
 #python assembler.py programa.asm programa.hex programa.bin
 def main(asm_file, hex_file, bin_file):
 
+    memory = MemoryManager()
     #primera pasada
     label_parser = ParserLabel()
     symbol_table = label_parser.get_labels(asm_file)
     print("pass 1 complete. Symbol Table:")
     print(symbol_table)
 
-    #segunda pasada
+    #segunda pasada: instrucciones
     lexer = RISCVLexer()
     parser = RISCVParser(symbol_table)
 
@@ -34,6 +36,9 @@ def main(asm_file, hex_file, bin_file):
         for instruction_code in machine_code:
             f_hex.write(f"{instruction_code:08x}\n")
             f_bin.write(f"{instruction_code:032b}\n")
+    
+    print("\n--- Data Section ---")
+    memory.dump_data()
 
     print(f"Assembly complete. Output files: {hex_file}, {bin_file}")
 
@@ -44,3 +49,4 @@ if __name__ == "__main__":
         print("Usage: python assembler.py <input.asm> <output.hex> <output.bin>")
     else:
         main(sys.argv[1], sys.argv[2], sys.argv[3])
+
