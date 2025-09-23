65c56f9ed1ead4819e242de9e9a812b3356fd94c
juanjore0 <juan.arango7@utp.edu.co>
2025-09-23 10:32:38 -0500

    cambios en parserPrincipal y parserlabel

--- programa.asm
+++ programa.asm
@@ -1,6 +1,24 @@
 .text
 main:
-    addi x1, x2, 10
-    lw x3, 0(x1)
-    sw x3, 4(x2) # store word
-    ecall
+    nop             # debería convertirse en addi x0, x0, 0
+    li x5, 10       # debería convertirse en addi x5, x0, 10
+    mv x6, x5       # debería convertirse en addi x6, x5, 0
+    not x7, x6      # debería convertirse en xori x7, x6, -1
+    neg x8, x7      # debería convertirse en sub x8, x0, x7
+    seqz x9, x8     # debería convertirse en sltiu x9, x8, 1
+    snez x10, x9    # debería convertirse en sltu x10, x0, x9
+    sltz x11, x10   # debería convertirse en slt x11, x10, x0
+    sgtz x12, x11   # debería convertirse en slt x12, x0, x11
+
+    beqz x5, etiqueta   # debería convertirse en beq x5, x0, etiqueta
+    bnez x6, etiqueta   # debería convertirse en bne x6, x0, etiqueta
+    blez x7, etiqueta   # debería convertirse en bge x0, x7, etiqueta
+    bgez x8, etiqueta   # debería convertirse en bge x8, x0, etiqueta
+    bltz x9, etiqueta   # debería convertirse en blt x9, x0, etiqueta
+    bgtz x10, etiqueta  # debería convertirse en blt x0, x10, etiqueta
+
+    j etiqueta      # debería convertirse en jal x0, etiqueta
+    ret             # debería convertirse en jalr x0, x1, 0
+
+etiqueta:
+    ecall           # syscall para terminar
